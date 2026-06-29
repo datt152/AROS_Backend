9aabf9bc9e599a6319f76397b3d98bfff2978d56
DatTran <trandat5079@gmail.com>
2026-06-29 17:51:23 +0700

    feat: add Test controller and add new dependency

---
+++ src/main/java/vn/edu/aros/aroscore/controller/TestController.java
@@ -0,0 +1,13 @@
+package vn.edu.aros.aroscore.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String testConnection() {
+        return "AROS Backend is running successfully!";
+    }
+}
