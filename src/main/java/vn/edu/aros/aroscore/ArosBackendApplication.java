139e919be9a36e024a5d53934dcc41cf6c52bbff
DatTran <trandat5079@gmail.com>
2026-06-29 17:33:07 +0700

    build(core): initialize project and configure postgresql database

---
+++ src/main/java/vn/edu/aros/aroscore/ArosBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.aros.aroscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArosBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/aros/aroscore/ArosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.aros.aroscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
